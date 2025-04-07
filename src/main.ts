f6f39f23926c25568c13dd1c6c2085b26c3a7ac9
unknown <kamenko.gennady@gmail.com>
2025-04-07 13:02:54 +0300

    add sentry metrics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { init } from '@sentry/angular';
+
+init({
+    dsn: 'https://36cdb8fee342b33eda1c968ea8f84075@o4508291284271104.ingest.de.sentry.io/4509111396335696',
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err),
